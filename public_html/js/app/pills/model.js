c13b151cd921914c2b5b386b08b3a8b589a85adc
Dmytro Kovalchuk <dimasmith@gmail.com>
2013-10-21 21:59:16 +0300

    add overriding of default attributes

--- public_html/js/app/pills/model.js
+++ public_html/js/app/pills/model.js
@@ -1,21 +1,22 @@
-define(["backbone"], function(backbone){   
-   var Pill = Backbone.Model.extend({       
-       parse: function(response){           
-           this.set("title", response.title);
-           this.set("doze", response.doze);
-           this.set("time", new Date(response.time));
-       }
-       
-   });
-   
-   var Prescriptions = Backbone.Collection.extend({
-       model: Pill,
-       url: "data/pills.json"
-   });
-   
-   return {
-       Pill: Pill,
-       Prescriptions: Prescriptions
-   };
+define(["backbone"], function(backbone) {
+    
+    var Pill = Backbone.Model.extend({
+        defaults: function() {
+            return {
+                title: "Pill",
+                doze: "doze"
+            };
+        }
+    });
+
+    var Prescriptions = Backbone.Collection.extend({
+        model: Pill,
+        url: "data/pills.json"
+    });
+
+    return {
+        Pill: Pill,
+        Prescriptions: Prescriptions
+    };
 });
 
--- public_html/js/app/pills/view.js
+++ public_html/js/app/pills/view.js
@@ -5,9 +5,9 @@ define(["backbone","jquery", "app/pills/model"], function(b, $, model){
     var PillView = Backbone.View.extend({
         tagName: "div",
         template: _.template($("#pill-template").html()),
-        
+                        
         render: function(){
-            this.$el.html(this.template(this.model.toJSON()));
+            this.$el.html(this.template(this.model));
             return this;
         }
     });
@@ -16,19 +16,14 @@ define(["backbone","jquery", "app/pills/model"], function(b, $, model){
         el: $("#prescriptions"),
         
         initialize: function(){
-            this.listenTo(recipes, "add", this.addOne);
-            this.listenTo(recipes, "change", this.render);
+            this.listenTo(recipes, "all", this.render);
             
             recipes.fetch();
-        },
-        addOne: function(pill){
-            var view = new PillView({model: pill});
-            this.$el.append(view.render().el);
-        },
+        },        
         render: function(){
             this.$el.empty();
             for(var i = 0; i < recipes.length; i++){
-                var view = new PillView({model: recipes.get(i)});
+                var view = new PillView({model: recipes.toJSON()[i]});
                 this.$el.append(view.render().el);                
             }
         }
